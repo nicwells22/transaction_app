fc3a71c0fffb695dd6db12e1993729dc6b9a4ab5
nicwells22 <nicwells22@gmail.com>
2019-09-05 10:55:53 -0500

    add db create transaction

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import cfg
 from sqlalchemy import create_engine
+from datetime import datetime
 import random
 
 def create_sql_engine():
@@ -31,14 +32,21 @@ def create_seller_location(location_id, seller_id, location_lat=None, location_l
     engine.dispose()
 
 
-def create_transcation():
-
+def create_transcation(transaction_id, location_id, buyer_id, sub_total, tax, total, transaction_lat=None, transaction_long=None):
+    engine = create_sql_engine()
+    transaction_lat = transaction_lat if transaction_lat else 'NULL'
+    transaction_long = transaction_long if transaction_long else 'NULL'
+    current_time = datetime.now()
+    r = engine.execute('''INSERT INTO transact (transaction_id, location_id, buyer_id, sub_total, tax, total, transaction_datetime, transaction_lat, transaction_long)
+                            VALUES ({id}, {location}, {buyer}, {sub_total}, {tax}, {total}, {time}, {lat}, {long})
+                            '''.format(id=transaction_id, location=location_id, buyer=buyer_id, sub_total=sub_total, tax=tax, total=total, time='NOW()', lat=transaction_lat, long=transaction_long))
+    engine.dispose()
 
 
 def create_item(item_id, transaction_id, item_name, item_common_name, item_cost):
     engine = create_sql_engine()
     r = engine.execute('''INSERT INTO items (item_id, transaction_id, item_name, item_common_name, item_cost)
-                            VALUES ({id}, {transact_id}, {name}, {common_name}, {cost})
+                            VALUES ({id}, {transact_id}, '{name}', '{common_name}', {cost})
                             '''.format(id=item_id, transact_id=transaction_id, name=item_name, common_name=item_common_name, cost=item_cost))
     engine.dispose()
 
@@ -49,11 +57,17 @@ if __name__ == '__main__':
     s = r + 1
     sl1 = r + 2
     sl2 = r + 3
-
+    t = r + 4
+    t2 = r + 5
+    i = r + 6
     create_buyer(b)
     create_seller(s, 'Nic Company')
     create_seller_location(sl1, s)
     create_seller_location(sl2, s, 33.33, 44.44)
+    create_transcation(t, sl1, b, 45.03, (45.03*0.07), 45.03 + (45.03*0.07), 55.55, -66.66)
+    create_transcation(t2, sl1, b, 45.03, (45.03*0.07), 45.03 + (45.03*0.07))
+    create_item(i, t, 'Golden Apple', 'Apple', 32)
+    create_item(i + 1, t, 'Ripe Pineapple', 'Pineapple', 13.03)
 
 # create seller
 
